fb7b768c3323a28132a02d519846095d35003f8d
Wei Kang <weikang9009@gmail.com>
2018-08-06 19:22:52 -0700

    geopandas in travis.yml

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import os.path
 from distutils.command.build_py import build_py
 
 # Get __version__ from PACKAGE_NAME/__init__.py without importing the package
-# __version__ has to be defined in the firt line
-exec(compile(open('PACKAGE_NAME/__init__.py').readline(),
-             'PACKAGE_NAME/__init__.py', 'exec'))
+# __version__ has to be defined in the first line
+with open('PACKAGE_NAME/__init__.py', 'r') as f:
+    exec (f.readline())
 
 def _get_requirements_from_files(groups_files):
     groups_reqlist = {}
