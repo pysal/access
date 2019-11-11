28ca86e2ca49a64e9eacdf01c2d6baa90225b3d6
jkoschinsky <jkoschinsky@uchicago.edu>
2019-11-11 14:35:43 -0600

    update

--- access/raam/raam.py
+++ access/raam/raam.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 
-
-def iterate_raam(demand, supply, travel, 
+def iterate_raam(demand, supply, travel,
                  max_cycles = 151, initial_step = 0.2, min_step = 0.005, half_life = 50,
                  limit_initial = 20, verbose = False):
 
@@ -11,10 +10,10 @@ def iterate_raam(demand, supply, travel,
     assignment[range(norig), travel.argmin(axis = 1)] = demand
 
 
-    for i in range(max_cycles): 
-            
-        demand_at_supply = assignment.sum(axis = 0) 
-        congestion_cost  = demand_at_supply / supply 
+    for i in range(max_cycles):
+
+        demand_at_supply = assignment.sum(axis = 0)
+        congestion_cost  = demand_at_supply / supply
         total_cost       = (congestion_cost + travel)
 
         max_locations = np.ma.masked_array(total_cost, assignment == 0).argmax(axis = 1)
@@ -36,7 +35,7 @@ def iterate_raam(demand, supply, travel,
 
         drlmin_new = ((slmin * slmax) / (slmin + slmax)) * \
                      ((trlmax - trlmin) + (dr + drotherlmax) / slmax - drotherlmin / slmin)
-        
+
         delta = drlmin_new - drlmin
 
         delta = np.minimum(delta, drlmax)
@@ -70,21 +69,21 @@ def iterate_raam(demand, supply, travel,
 
             delta = delta_mat.sum(axis = 1)
 
-            
+
 
         assignment[range(norig), min_locations] += delta
         assignment[range(norig), max_locations] -= delta
-        
+
         assert((assignment.sum(axis = 1) == demand).all())
-        
+
         if not (i % 25):
             raam_cost = (total_cost * assignment).sum(axis = 1) / assignment.sum(axis = 1)
 
-            if verbose: 
+            if verbose:
                 print("{:d} {:.2f} {:d} {:.3f}".format(i, raam_cost.mean(), delta.sum(), step_size), end = " || ")
 
     raam_cost = (total_cost * assignment).sum(axis = 1) / assignment.sum(axis = 1)
-    
+
     return raam_cost
 
 
@@ -96,20 +95,20 @@ def raam(demand_df, supply_df, cost_df,
          demand_index = True, demand_name = "demand",
          supply_index = True, supply_name = "supply",
          cost_origin   = "origin", cost_dest = "dest", cost_name = "cost",
-         tau = 60, rho = None, 
+         tau = 60, rho = None,
          max_cycles = 150, initial_step = 0.2, min_step = 0.005, half_life = 50,
          verbose = False):
     """
-    Calculate the rational agent access model's total cost -- 
+    Calculate the rational agent access model's total cost --
       a weighted travel and congestion cost.
     The balance of the two costs is expressed by the
-      $\tau$ parameter, which corresponds to the travel time 
+      $\tau$ parameter, which corresponds to the travel time
       required to accept of congestion by 100% of the mean demand to supply ratio
       in the study area.
-  
+
     Parameters
     ----------
-  
+
     demand_df     : [pandas.DataFrame](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.html)
                     The origins dataframe, containing a location index and a total demand.
     demand_origin : str
@@ -138,35 +137,35 @@ def raam(demand_df, supply_df, cost_df,
     max_cost   : float
                   This is the maximum cost to consider in the weighted sum;
                     note that it applies _along with_ the weight function.
-  
+
     Returns
     -------
     access     : pandas.Series
-        
-                  A -- potentially-weighted -- Rational Agent Access Model cost.
+
+                  A -- potentially-weighted -- Rational Agengmailt Access Model cost.
     """
 
     if demand_index is not True: demand_df = demand_df.set_index(demand_index)
     if supply_index is not True: supply_df = supply_df.set_index(supply_index)
-    
+
     demand_df = demand_df[demand_df[demand_name] > 0].copy()
     supply_df = supply_df[supply_df[supply_name] > 0].copy()
 
     demand_locations = list(set(cost_df[cost_origin]) & set(demand_df.index))
     supply_locations = list(set(cost_df[cost_dest])   & set(supply_df.index))
-    
+
     cost_pivot = cost_df.pivot(index=cost_origin, columns=cost_dest, values=cost_name)
     travel_np  = cost_pivot.loc[demand_locations, supply_locations].to_numpy().copy()
     travel_np  = travel_np / tau
     travel_np  = np.ma.masked_array(travel_np, np.isnan(travel_np))
-    
+
     # If it is not specified, rho is the average demand to supply ratio.
     if rho is None: rho = demand_df[demand_name].sum() / supply_df[supply_name].sum()
-    
+
     supply_np = supply_df.loc[supply_locations, supply_name].to_numpy().copy()
-    supply_np = supply_np * rho 
-    
-    # Change this -- should be 
+    supply_np = supply_np * rho
+
+    # Change this -- should be
     demand_np = demand_df.loc[demand_locations, demand_name].to_numpy().copy()
 
     raam_cost = iterate_raam(demand_np, supply_np, travel_np, verbose = verbose,
@@ -176,8 +175,3 @@ def raam(demand_df, supply_df, cost_df,
     rs = pd.Series(name = "RAAM", index = demand_locations, data = raam_cost)
 
     return rs
-
-
-
-
-
