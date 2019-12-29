1a01da707154c324cc078bb113e6086b7a5f1af3
vidal-anguiano <vidal.anguiano@gmail.com>
2019-12-29 17:14:29 -0600

    adds type and value errors to step_fn and gaussian

--- access/weights/weights.py
+++ access/weights/weights.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 def step_fn(step_dict):
+    if type(step_dict) != dict:
+        raise TypeError('step_dict must be of type dict.')
+
     for v in step_dict.values():
         if v < 0:
             raise ValueError('All weights must be positive.')
@@ -13,6 +16,8 @@ def step_fn(step_dict):
     return helper
 
 def gaussian(width):
+  if width == 0:
+      raise ValueError("Width must be non-zero.")
 
   return lambda x: np.exp(-x*x / (2 * width**2)) \
                    / np.sqrt(2*np.pi*width**2)
