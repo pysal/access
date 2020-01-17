098948786c21128b42cd5986a0bab94813c80aeb
vidal-anguiano <vidal.anguiano@gmail.com>
2020-01-16 23:51:26 -0600

    one more test fix

--- access/tests/test_floating_catchment_area.py
+++ access/tests/test_floating_catchment_area.py
@@ -56,11 +56,10 @@ class TestFloatingCatchmentArea(unittest.TestCase):
 
 
     def test_floating_catchment_area_ratio_warns_if_demand_supply_locs_differ_and_noise(self):
-        new_dem_row = pd.DataFrame([[1,1,1,None],[1,1,1,None]], columns=['x', 'y', 'value', 'geometry'])
-        self.model.demand_df.append(new_dem_row)
-        # diff = len(set(self.model.demand_df.index.tolist()) - set(self.model.supply_df[''].unique()))
+        new_dem_row = pd.DataFrame([[1,1,1,None],[1,1,1,None]], columns=['x', 'y', 'value', 'geometry'], index=[28,29])
+        self.model.demand_df = self.model.demand_df.append(new_dem_row)
+        self.model.demand_df.index.name = 'id'
         result = self.model.fca_ratio(noise=True)
-        # self.assertEqual(diff, 0)
 
     def test_floating_catchment_area_ratio_overwrites_column(self):
         small_catchment = .9
