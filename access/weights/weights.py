ee1d3f207993489c78efde648179e41f95b4ed0d
Jamie Saxon <james.saxon@gmail.com>
2020-05-19 14:02:39 -0500

    doc strings for weight functions

--- access/weights/weights.py
+++ access/weights/weights.py
@@ -1,6 +1,37 @@
 import numpy as np
 
 def step_fn(step_dict):
+    """
+    Create a step function from a dictionary.
+
+    Parameters
+    ----------
+    step_dict           : dict
+                          Dictionary of cut-offs and weight values.
+
+    Returns
+    -------
+
+    weight_function     : function
+                          Function returning weight, for input distance or time, _x_.
+                          Values beyond the largest threshold will return 0.
+
+    Examples
+    --------
+
+    Import the weights:
+
+    >>> from access import weights
+
+    Create a step function with thresholds at 20, 40, and 60.
+    Travel costs are in minutes here, but the code cannot tell if you mix units!
+
+    >>> fn = weights.step_fn({20 : 1, 40 : 0.68, 60 : 0.22})
+
+    >>> {v : fn(v) for v in range(0, 71, 10)}
+    {0: 1, 10: 1, 20: 1, 30: 0.68, 40: 0.68, 50: 0.22, 60: 0.22, 70: 0}
+    """
+
     if type(step_dict) != dict:
         raise TypeError('step_dict must be of type dict.')
 
@@ -9,19 +40,103 @@ def step_fn(step_dict):
             raise ValueError('All weights must be positive.')
 
     def helper(key_to_test):
+
         for k,v in sorted(step_dict.items()):
             if key_to_test <= k:
                 return v
+
         return 0
+
     return helper
 
-def gaussian(width):
-  if width == 0:
-      raise ValueError("Width must be non-zero.")
+def gaussian(sigma):
+    """
+    Create a gaussian weight function, for a specified width, :math:`\sigma`.
+    The mean / location parameter is assumed to be 0.
+    Note that the standard normalization of the Gaussian, :math:`1 / \sqrt{2\pi\sigma^2}`,
+    is _not_ applied, so :math:`f(0) = 1` regardless of the value of sigma..
+    Of course, this effect does not matter if the ultimate access values are normalized.
 
-  return lambda x: np.exp(-x*x / (2 * width**2)) \
-                   / np.sqrt(2*np.pi*width**2)
+    Parameters
+    ----------
+    sigma               : float 
+                          This the classical width parameter of the Gaussian / Normal distriution.
 
-def gravity(scale = 1, alpha = -1):
+    Returns
+    -------
 
-  return lambda x: np.power(x / scale, alpha)
+    weight_function     : function
+                          Function returning weight, for input distance or time, _x_.
+
+    Examples
+    --------
+
+    Import the weights.
+
+    >>> from access import weights
+
+    Create a step function with thresholds at 20, 40, and 60.
+    Travel costs are in minutes here, but the code cannot tell if you mix units!
+
+    >>> fn = weights.gaussian(sigma = 20)
+
+    >>> {v : fn(v) for v in range(0, 61, 20)}
+    {0: 1.0, 20: 0.6065306597126334, 40: 0.1353352832366127, 60: 0.011108996538242306}
+
+    Compare this to a simpler formulation:
+
+    >>> import numpy as np
+    >>> {x : np.exp(-x**2/2) for x in range(4)}
+    {0: 1.0, 1: 0.6065306597126334, 2: 0.1353352832366127, 3: 0.011108996538242306}
+    """
+
+    if sigma == 0:
+        raise ValueError("Width must be non-zero.")
+  
+    return lambda x: np.exp(-x*x / (2 * sigma**2)) # / np.sqrt(2*np.pi*sigma**2)
+
+
+def gravity(scale, alpha, min_dist = 0):
+    """
+    Create a gravity function from a scale :math:`s` and :math:`\\alpha` parameters
+    as well as an optional minimum distance :math:`\\underbar{x}`.
+    The function is of the form :math:`f(x) = (\\text{max}(x, \\underbar{x})/s)^\\alpha`.
+    Note that there is no overall normalization.
+
+    Parameters
+    ----------
+    scale               : float
+                          Scaling value, normalizing the function input.
+    alpha               : float
+                          Power to which the normalized inputs are raised.
+                          Note that it is _not_ implicitly negative (i.e., :math:`x^\\alpha` instead of :math:`1/x^\\alpha`.
+    min_dist            : float
+                          A 'standard' issue with gravity model is the infinite potential at 0 distance or time.
+                          This can be rectified crudely by specifying a minimum distance, 
+                          and setting any input exceeding that minimum to the minimum itself.
+                          The default threshold is 0.
+
+    Returns
+    -------
+
+    weight_function     : function
+                          Function returning weight, for input distance or time, _x_.
+
+    Examples
+    --------
+
+    Import the weights:
+
+    >>> from access import weights
+
+    Create a step function with thresholds at 20, 40, and 60.
+    Travel costs are in minutes here, but the code cannot tell if you mix units!
+
+    >>> fn = weights.gravity(scale = 20, alpha = -2, min_dist = 1)
+
+    >>> {t : round(fn(t), 2) for t in [0, 1, 2, 20, 40, 60]}
+    {0: 400.0, 1: 400.0, 2: 100.0, 20: 1.0, 40: 0.25, 60: 0.11}
+    """
+
+    return lambda x: np.power(max(x, min_dist) / scale, alpha)
+
