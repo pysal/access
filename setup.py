6d5fa2248a71898c24db9661a5450b7577aa40d1
eli knaap <ek@knaaptime.com>
2022-01-27 22:33:56 -0800

    update py version in setup

--- setup.py
+++ setup.py
@@ -48,8 +48,10 @@ def setup_package():
             'Topic :: Scientific/Engineering :: GIS',
             'License :: OSI Approved :: BSD License',
             'Programming Language :: Python',
-            'Programming Language :: Python :: 3.5',
-            'Programming Language :: Python :: 3.6'
+            'Programming Language :: Python :: 3.7'
+            'Programming Language :: Python :: 3.8'
+            'Programming Language :: Python :: 3.9'
+            'Programming Language :: Python :: 3.10'
             ],
           license='3-Clause BSD',
           packages=find_packages(),
