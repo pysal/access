7c9e37e199b0099892c83549cf1cd3685a4dd4cd
Martin Fleischmann <martin@martinfleischmann.net>
2022-01-30 19:41:19 +0000

    REF: load datasets from the URL on request

--- access/tests/test_datasets.py
+++ access/tests/test_datasets.py
@@ -1,38 +1,18 @@
-import os
-import sys
-sys.path.append('../..')
-
-import math
 import unittest
 
-import numpy as np
 import pandas as pd
 import geopandas as gpd
 from access.datasets import Datasets
-import util as tu
 
 
 class TestDatasets(unittest.TestCase):
-
-    def test_file_download(self):
-        file_name = 'chi_times'
-        file_path = os.path.join(Datasets._dir_path, Datasets._datasets[file_name])
-        if os.path.exists(file_path):
-            os.remove(file_path)
-
-        Datasets.load_data(file_name)
-
-        actual = os.path.exists(file_path)
-
-        self.assertEqual(actual, True)
-
-
     def test_load_geopandas_dataset(self):
-        result = Datasets.load_data('chi_doc_geom')
-        actual = type(result) == gpd.geodataframe.GeoDataFrame
+        result = Datasets.load_data("chi_doc_geom")
+        assert isinstance(result, gpd.GeoDataFrame)
 
-        self.assertEqual(actual, True)
-
+    def test_load_pandas_dataset(self):
+        result = Datasets.load_data("chi_times")
+        assert isinstance(result, pd.DataFrame)
 
     def test_prints_available_datasets(self):
         Datasets.available_datasets()
