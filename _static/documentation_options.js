6ba3ad7a6728b145c7c948117c264d3b405a3c02
jGaboardi <jGaboardi@users.noreply.github.com>
2025-12-12 15:24:06 +0000

    deploy: 095fd95eb65bdaeb515696af6751ce059b7a5241

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g7149c70',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.1.10',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
